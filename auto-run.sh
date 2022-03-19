e2b8204bafde1a1d489d2d6649da51da60174e0d
Tim Koers <tim.koers@live.nl>
2022-03-19 12:18:50 +0100

    WHoopsie

--- auto-run.sh
+++ auto-run.sh
@@ -71,7 +71,7 @@ cp hostapd.conf /etc/hostapd/hostapd.conf
 
 # (re)Start the various daemons
 
-systemctl net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
